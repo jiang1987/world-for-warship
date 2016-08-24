639561048ccf0f917a426f15c6a4e1ebbd578e04
jiang1987 <429237623@qq.com>
2016-08-24 17:11:46 +0800

    change1

--- 江帆/0028116054033/views/register/register.js
+++ 江帆/0028116054033/views/register/register.js
@@ -2,7 +2,9 @@
  * Created by hxsd on 2016/8/6.
  */
 angular.module("myApp")
-    .controller("registerCtrl",function($scope){
-    $scope.email='';
-    $scope.password='';
-});
+    .controller("registerCtrl", function ($scope) {
+        $scope.clear = function () {
+            $scope.email=null;
+            $scope.password=null;
+        };
+    });
